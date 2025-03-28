d76b9f8fdf94b23bc048a13be450e351e94aae3e
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2025-03-28 22:29:54 +0530

    added external package to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
-})
+  base: '/',
+  build: {
+    rollupOptions: {
+      external: ['html2canvas']
+    }
+  }
+});
