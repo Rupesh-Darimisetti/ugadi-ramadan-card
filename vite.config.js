72b838017ce7b0efdaab424fc4e16f4779618f7f
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2025-03-28 22:42:59 +0530

    add the html2canvas properly to config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['html2canvas']
-    }
-  }
+      external: ['html2canvas'],
+    },
+  },
 });
